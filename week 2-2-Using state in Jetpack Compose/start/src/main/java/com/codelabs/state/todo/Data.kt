6c010c72c40009cae9e8ef942f041f2e511e490b
ysjoung <jeongysu@gmail.com>
2021-11-19 17:35:47 +0900

    week2-2 done!

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/Data.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/Data.kt
@@ -18,14 +18,10 @@ package com.codelabs.state.todo
 
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CropSquare
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material.icons.filled.Event
-import androidx.compose.material.icons.filled.PrivacyTip
-import androidx.compose.material.icons.filled.RestoreFromTrash
+import androidx.compose.material.icons.filled.*
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.codelabs.state.R
-import java.util.UUID
+import java.util.*
 
 data class TodoItem(
     val task: String,
@@ -35,7 +31,7 @@ data class TodoItem(
 )
 
 enum class TodoIcon(val imageVector: ImageVector, @StringRes val contentDescription: Int) {
-    Square(Icons.Default.CropSquare, R.string.cd_expand),
+    Square(Icons.Default.CropSquare, R.string.cd_crop_square),
     Done(Icons.Default.Done, R.string.cd_done),
     Event(Icons.Default.Event, R.string.cd_event),
     Privacy(Icons.Default.PrivacyTip, R.string.cd_privacy),
