d53835db37e78d4a76b48f3655599a9fd792f07d
ysjoung <jeongysu@gmail.com>
2021-11-19 17:11:05 +0900

    bug fixed

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/joung/week2_layout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/joung/week2_layout/MainActivity.kt
@@ -3,6 +3,8 @@ package com.joung.week2_layout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -14,6 +16,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,31 +39,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NameTag() {
-    Row{
+    Row(
+    ){
         Surface(
             modifier = Modifier
                 .size(50.dp)
-                .padding(all = 4.dp),
+                .padding(all = 4.dp)
+                .clip(RoundedCornerShape(4.dp)),
             shape = CircleShape,
             color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
         ) {
-            // image
+            // image goes here
         }
-    }
-    Column (
-        modifier = Modifier
-            .padding(all = 8.dp)
-            .align(Alignment.CenterVertically)
-            .clip(RoundedCornerShape(4.dp))
-            ){
-        Text(text = "Joung", fontWeight = FontWeight.Bold)
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Text(text = "PHONE NUMBER", style = MaterialTheme.typography.body2)
+        Column (
+            modifier = Modifier
+                .padding(all = 8.dp)
+                .align(Alignment.CenterVertically)
+        ){
+            Text(text = "Joung", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "PHONE NUMBER", style = MaterialTheme.typography.body2)
+            }
         }
     }
 }
-
-
 
 @Preview(showBackground = true)
 @Composable
