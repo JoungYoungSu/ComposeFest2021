c6ffdfd6c98e0c7726846accc2e070c54aa05eb4
ysjoung <jeongysu@gmail.com>
2021-11-19 15:50:41 +0900

    writing codes... have troubling in Modifier.align suppose some kind of import error or version error

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/joung/week2_layout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/joung/week2_layout/MainActivity.kt
@@ -3,11 +3,20 @@ package com.joung.week2_layout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.joung.week2_layout.ui.theme.Week2LayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +26,7 @@ class MainActivity : ComponentActivity() {
             Week2LayoutTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    NameTag()
                 }
             }
         }
@@ -25,14 +34,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NameTag() {
+    Row{
+        Surface(
+            modifier = Modifier
+                .size(50.dp)
+                .padding(all = 4.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // image
+        }
+    }
+    Column (
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .align(Alignment.CenterVertically)
+            .clip(RoundedCornerShape(4.dp))
+            ){
+        Text(text = "Joung", fontWeight = FontWeight.Bold)
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(text = "PHONE NUMBER", style = MaterialTheme.typography.body2)
+        }
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun CardPreview() {
     Week2LayoutTheme {
-        Greeting("Android")
+        NameTag()
     }
 }
